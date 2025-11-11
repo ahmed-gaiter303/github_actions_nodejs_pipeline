0cf10f28944bc06698fd20d2b27302900d23ccc2
Ahmed Ekramy <ahmedekramy303@gmail.com>
2025-11-12 00:42:18 +0200

    Refactor app to use HTTP server instead of Express

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Hello from Node.js CI/CD!'));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('CI/CD pipeline and Docker are working!\n');
+});
 
-app.listen(3000, () => console.log('App running on port 3000'));
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
